a3bf47cba6c7ae0573c0c94c7c02975e0bb7dc7f
lotus99799 <sanket.gondaliya2002@gmail.com>
2026-04-09 17:53:03 +0530

    next.config: update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.1.5","https://my-workout-app-2.netlify.app/"],
+  allowedDevOrigins: ["192.168.1.5","https://my-workout-app-2.netlify.app/", "https://my-workout-app-beta.vercel.app"],
 };
 
 export default nextConfig;
